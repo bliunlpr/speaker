ee44ad6190325d3a17d7ba3518250364c9c7e2c8
bliunlpr <liubin2015@ia.ac.cn>
2018-10-31 17:54:38 +0800

    Add files via upload

--- run.sh
+++ run.sh
@@ -3,6 +3,9 @@
 stage=0
 seq_training=false
 train_type="multi_attention"
+model_type="lstm"
+segment_type="none"
+speaker_num=64
 . local/parse_options.sh  # e.g. this parses the --stage option if supplied.
 
 
@@ -24,6 +27,7 @@ fi
 
 if [ $stage -le 2 ]; then
   
-  python3 local/main.py --dataroot data --seq_training $seq_training --train_type $train_type --name speaker_seq_"$seq_training"_$train_type|| exit 1;
+  python3 local/main.py --dataroot data --seq_training $seq_training --train_type $train_type --model_type $model_type --segment_type $segment_type \
+                        --speaker_num $speaker_num --name speaker_seq_"$seq_training"_"$model_type"_"$train_type"_"$segment_type"
   
 fi
